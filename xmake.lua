c0ab8c21a42e2f874fc0cf7e8353052a365beb33
LWenK <m@lwenk.com>
2025-06-20 20:29:15 +0800

    feat: adapt to LeviLamina 1.3.1

--- xmake.lua
+++ xmake.lua
@@ -6,12 +6,12 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
 if is_config("target_type", "server") then
-    add_requires("levilamina 1.2.1", {configs = {target_type = "server"}})
+    add_requires("levilamina 1.3.1", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 1.2.1", {configs = {target_type = "client"}})
+    add_requires("levilamina 1.3.1", {configs = {target_type = "client"}})
 end
 
-add_requires("levibuildscript")
+add_requires("levibuildscript 0.4.0")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
